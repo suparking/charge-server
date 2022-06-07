b2b8725eb1decf8e4518e725d51e2c3f4f10659a
nuo-promise <nuoyan@apache.org>
2022-06-07 17:44:29 +0800

    init

---
+++ src/main/java/net/suparking/chargeserver/ChargeServerApplication.java
@@ -0,0 +1,13 @@
+package net.suparking.chargeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/suparking/chargeserver/ChargeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.suparking.chargeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
